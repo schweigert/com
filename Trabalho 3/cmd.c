5e34b00f50c317f8f0047dd1c0a1f1d378bddc4c
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-06-02 08:32:47 -0300

    CmdGenerate ISTORE

--- Trabalho 3/cmd.c
+++ Trabalho 3/cmd.c
@@ -1,4 +1,7 @@
+#include <stdio.h>
 #include "cmd.h"
+#include "eda.h"
+
 
 CMD CmdList[MAX_COMMAND] = {0};
 int CmdIndex = 0;
@@ -9,4 +12,11 @@ void cmdGenerate(int command, int arg1, int arg2)
   CmdList[CmdIndex].command = command;
   CmdList[CmdIndex].arg1 = arg1;
   CmdList[CmdIndex].arg2 = arg2;
+  CmdIndex++;
+}
+
+void CmdIstore (struct arvore* arv, char* var)
+{
+  int posicao = buscaPosicao(arv, var);
+  cmdGenerate()
 }
--- Trabalho 3/expr.y
+++ Trabalho 3/expr.y
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "eda.h"
+#include "cmd.h"
 
 typedef struct {
     struct List *listaId;
