a09e1167370732c36395987dd6273119493cd445
fleyhe <fleyhe0@gmail.com>
2017-06-12 14:00:53 -0300

    Incremento por ID

---
+++ Trabalho 6/cmd.h
@@ -0,0 +1,100 @@
+#ifndef _CMD_N_DEF_
+#define _CMD_N_DEF_
+
+#include "eda.h"
+
+#define MAX_COMMAND 10000
+#define MAX_FUNCS 1000
+
+typedef struct {
+  int label;
+  int command;
+  int arg1;
+  int arg2;
+} CMD;
+
+// Commands               #Bin ID   #Arg1   #Arg2
+#define CMD_ISTORE        0x0000 // Posi    --
+#define CMD_BIPUSH        0x0001 // Number  --
+#define CMD_IADD          0x0002 // --      --
+#define CMD_IMULL         0x0003 // --      --
+#define CMD_ILOAD         0x0004 // Posi    --
+#define CMD_ICONST_1      0x0005 // --      --
+#define CMD_ICONST_2      0x0006 // --      --
+#define CMD_ICONST_3      0x0007 // --      --
+#define CMD_ICONST_4      0x0008 // --      --
+#define CMD_ICONST_5      0x0009 // --      --
+#define CMD_IDIV          0x000A // --      --
+#define CMD_INEG          0x000B // --      --
+#define CMD_IREM          0x000C // --      --
+#define CMD_ISHL          0x000D // --      --
+#define CMD_ISHR          0x000E // --      --
+#define CMD_ISUB          0x000F // --      --
+#define CMD_GSOUT         0x0010 // --      -- -> getstatic java/lang/System/out Ljava/io/PrintStream;
+#define CMD_INVKOUTI      0x0011 // --      -- -> invokevirtual java/io/PrintStream/println(I)V
+#define CMD_INVKOUTL      0x0012 // --      -- -> invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V
+#define CMD_LDC           0x0013 // "str"   -- -> impilha o ponteiro para a string
+#define CMD_LDCN          0x0014 // n       -- -> Empilha uma constante maior
+#define CMD_IF_ICOMPEQ    0x0015 // Label   --
+#define CMD_IF_ICOMPNE    0x0016 // Label   --
+#define CMD_IF_ICOMPLT    0x0017 // Label   --
+#define CMD_IF_ICOMPLE    0x0018 // Label   --
+#define CMD_IF_ICOMPGT    0x0019 // Label   --
+#define CMD_IF_ICOMPGE    0x001A // Label   --
+#define CMD_GOTO          0x001B // Label   --
+
+void showCmd();
+void ShowCmdAscii();
+
+void showLiteralTable();
+
+void cmdGenerate(int, int, int);
+
+// Lista de Comandos para tratar Label
+
+int LabelCreate();
+void LabelUpdate(struct IntList* lista, int label);
+int GetIndexPosition();
+
+// Lista de Comandos em Pilha para a JVM
+void CmdIstore (struct arvore* arv, char* var);
+void CmdIadd ();
+void CmdImull ();
+void CmdBipush (int value);
+void CmdIload (struct arvore* arv, char* var);
+void CmdIdiv ();
+void CmdIneg ();
+void CmdIrem ();
+void CmdIshl ();
+void CmdIshr ();
+void CmdIsub ();
+void CmdGetStaticOut ();
+void CmdInvokeOutInt ();
+void CmdInvokeOutLiteral ();
+void CmdLdc (char*);
+void CmdLdcn (int);
+
+void CmdIfEQ();
+void CmdIfNE();
+void CmdIfLT();
+void CmdIfLE();
+void CmdIfGT();
+void CmdIfGE();
+
+void CmdGOTO();
+void CmdGOTOLabel(int);
+
+// Operações de chamada do Bytecode
+void loadToMain();
+
+// Operações do Assembler
+void writeCmds();
+void writeJasminInit(char* file);
+void writeJasminBody();
+void writeJasminMain();
+void writeJasminExit();
+
+void callJasmin();
+void rmJasminOut();
+
+#endif
